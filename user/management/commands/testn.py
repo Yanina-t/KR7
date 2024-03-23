55c1b859201eddb9021384ba12c77e5115245fc6
Yanina <test@test.ru>
2024-03-23 10:49:50 +0100

    123

--- user/management/commands/testn.py
+++ user/management/commands/testn.py
@@ -1,10 +1,14 @@
 from django.core.management import BaseCommand
 
-from user.utils import MyBot
+from user.utils import MyBot, send_registration_confirmation_email
 
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        my_bot = MyBot()
-        my_bot.send_message('Hi , test bot =)')
+        # my_bot = MyBot()
+        # my_bot.send_message('Hi , test bot =)')
+
+        send_registration_confirmation_email('yaninatest3@gmail.com')
+
+
